66743de92c88cc0da1721933b4930eaae12763be
clb <295435732@qq.com>
2019-04-02 12:36:52 +0800

    second

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="login">
+    <input type="text" v-model="username">
+    <input type="password" v-model="password">
+    <input type="button" value="登录" v-on:click="loginActive()">
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "login",
+      data () {
+        return {
+          username: 'Welcome to Your Vue.js App',
+          password:"******"
+        }
+      },
+      // 点击菜单使用的函数
+      methods: {
+        loginActive: function(){
+         var string= this.username+this.password;
+         alert(string);
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
